db9d73fb12e228576ea05fe4aee43d58f18e4765
MIMOmaram <marayari123@gmail.com>
2024-06-12 13:17:40 +0100

    first commit

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,70 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/profile/user.service';
-import { forkJoin } from 'rxjs';
 import { DashboardService } from '../../services/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrls: ['./content.component.css']
 })
-export class ContentComponent  implements OnInit{
+export class ContentComponent implements OnInit {
 
+  userProfile: any = null;
+  userId: number | undefined;
 
- userProfile: any = null;
- userId: number | undefined;
-  constructor(  private router: Router,private userService:UserService,private dashboardService: DashboardService) {}
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private dashboardService: DashboardService
+  ) {}
+
   ngOnInit(): void {
+    this.loadUserProfile();
 
+    // Reload the entire page every hour (3600000 milliseconds)
+    setInterval(() => {
+      window.location.reload();
+    }, 3600000);
+  }
 
-
-
-      this.userService.getUserProfile().subscribe(
-        (data: any) => {
-          this.userProfile = data;
-          this.userId = this.userProfile.id;
-        },
-        (error: any) => {
-          console.error('Error fetching user profile:', error);
-        }
-      );
-
-    }
+  loadUserProfile(): void {
+    this.userService.getUserProfile().subscribe(
+      (data: any) => {
+        this.userProfile = data;
+        this.userId = this.userProfile.id;
+      },
+      (error: any) => {
+        console.error('Error fetching user profile:', error);
+      }
+    );
+  }
 
   redirectToHome() {
     this.router.navigate(['/homee']);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
 }
-
